6d996b0a422b88945d2e46e209f433339b1dc8da
TiliaSNogueira <tiliasalomao@gmail.com>
2020-11-15 15:52:42 -0300

    fragment ExibeSeries

--- app/src/main/java/com/e/jarvis/ExibePersonagemFragment.kt
+++ app/src/main/java/com/e/jarvis/ExibePersonagemFragment.kt
@@ -3,6 +3,8 @@ package com.e.jarvis
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_exibe_personagem.view.*
 
 class ExibePersonagemFragment : Fragment() {
 
@@ -13,6 +15,11 @@ class ExibePersonagemFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_exibe_personagem, container, false)
+
+        view.frag_personagem_btn_char.setOnClickListener() {
+            findNavController().navigate(R.id.action_exibePersonagemFragment2_to_exibeSeriesFragement)
+        }
+
 
 
         return view
