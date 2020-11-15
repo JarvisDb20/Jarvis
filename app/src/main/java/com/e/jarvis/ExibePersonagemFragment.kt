ef73d85744c0e1294fb3ebf0b7186f549c41c369
TiliaSNogueira <tiliasalomao@gmail.com>
2020-11-14 21:17:36 -0300

    toolbar e navigation

--- app/src/main/java/com/e/jarvis/ExibePersonagemFragment.kt
+++ app/src/main/java/com/e/jarvis/ExibePersonagemFragment.kt
@@ -1,19 +1,23 @@
 package com.e.jarvis
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 class ExibePersonagemFragment : Fragment() {
 
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exibe_personagem, container, false)
+        val view = inflater.inflate(R.layout.fragment_exibe_personagem, container, false)
+
+
+        return view
+
+
     }
 
 
